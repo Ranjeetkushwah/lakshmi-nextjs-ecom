413c9da27ec954d0f0883d2ce375e49056368e6b
Ranjeet Kushwah <ranjitkush25@gmail.com>
2025-10-09 12:14:04 +0530

    feat: update admin dashboard and sidebar with collapsible menu functionality

--- components/Application/Admin/AppSidebar.jsx
+++ components/Application/Admin/AppSidebar.jsx
@@ -9,6 +9,9 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
 } from "@/components/ui/sidebar"
 import Image from 'next/image'
 import logoBlack from '@/public/assets/images/logo-black-ecom.png'
@@ -17,26 +20,63 @@ import { Button } from '@/components/ui/button'
 import { LuChevronRight } from "react-icons/lu";
 import { IoMdClose } from "react-icons/io";
 import { adminAppSidebarMenu } from '@/lib/adminSidebarMenu'
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible'
+import Link from 'next/link'
+
+
 
 function AppSidebar() {
   return (
     <Sidebar>
       <SidebarHeader className='border-b h-17 p-0' >
         <div className='flex justify-between items-center px-4 '>
-            <Image src={logoBlack.src} height={50} width={logoBlack.width}  className='block dark:hidden h-[80px] w-auto' alt='logo dark' />
-            <Image src={logoWhite.src} height={50} width={logoWhite.width} className='hidden dark:block h-[80px] w-auto'  alt='logo white' />
-            <Button type='button' size='icon' className='bg-blue-600 hover:bg-blue-500'>
-                <IoMdClose/>
-            </Button>
+          <Image src={logoBlack.src} height={50} width={logoBlack.width} className='block dark:hidden h-[80px] w-auto' alt='logo dark' />
+          <Image src={logoWhite.src} height={50} width={logoWhite.width} className='hidden dark:block h-[80px] w-auto' alt='logo white' />
+          <Button type='button' size='icon' className='bg-blue-600 hover:bg-blue-500'>
+            <IoMdClose />
+          </Button>
         </div>
       </SidebarHeader>
-      <SidebarContent>
+      <SidebarContent className='p-3'>
         <SidebarMenu>
-          {/* {
-            adminAppSidebarMenu.map((menu,index)=>(
+          {adminAppSidebarMenu.map((menu, index) => (
+            <Collapsible key={index} className='group/collapsible'>
+              <SidebarMenuItem>
+                <CollapsibleTrigger asChild>
+                  <SidebarMenuButton asChild className='font-semibold px-2 py-5'>
+                    <Link href={menu?.url}>
+                      <menu.icon />
+                      {menu.title}
+                      {
+                        menu.submenu && menu.submenu.length > 0 &&
+                        <LuChevronRight className='ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90' />
+                      }
+                    </Link>
+                  </SidebarMenuButton>
+                </CollapsibleTrigger>
+                {menu.submenu && menu.submenu.length > 0 &&
+                  <CollapsibleContent>
+                    <SidebarMenuSub>
+                      {
+                        menu.submenu.map((submenuItem, submenuIndex) => (
+                          <SidebarMenuSubItem key={submenuIndex}>
+                            <SidebarMenuSubButton asChild className=' px-2 py-5'>
+                              <Link href={submenuItem.url}>
+                                {submenuItem.title}
+                              </Link>
+                            </SidebarMenuSubButton>
+                          </SidebarMenuSubItem>
+                        ))
+                      }
+                    </SidebarMenuSub>
 
+                  </CollapsibleContent>
+                }
+
+              </SidebarMenuItem>
+            </Collapsible>
             ))
-          } */}
+          }
         </SidebarMenu>
       </SidebarContent>
     </Sidebar>
